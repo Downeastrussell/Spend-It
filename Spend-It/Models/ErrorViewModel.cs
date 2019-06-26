e056db34f61d7e2a1d18deabf5dfb575df2ccb9a
Russell Miller <bet2buy@gmail.com>
2019-06-26 10:08:03 -0400

    creating repo

---
+++ Spend-It/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spend_It.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
